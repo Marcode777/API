5db6d28ce7f5fb2be73550c5e670b2a0f1c92d69
Marcode777 <marc102202@hotmail.com>
2016-03-12 12:04:19 -0500

    modified api.js, home.html

--- api.js
+++ api.js
@@ -2,6 +2,15 @@ $(document).ready(function(){
   $.ajax({
     type: "GET",
     url: "https//api.github.com/orgs/facebook/repos",
-    success: function(repos)
+    success: function(repos){
+      console.log("this is works");
+    },
+      error: function(jqXHR, textStatus, erroThrown){
+        alert("this works")
+      }
   })
 })
+
+
+
+//use darksky or googlemaps instead of twitter because they will not require authentication 
